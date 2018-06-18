eea2e33dc4362e6cc38af43f6481cc6bc361f3ce
yxmysky <yx922011@163.com>
2018-06-18 22:23:55 +0800

    创建项目

---
+++ DouYuZB/DouYuZBTests/DouYuZBTests.swift
@@ -0,0 +1,36 @@
+//
+//  DouYuZBTests.swift
+//  DouYuZBTests
+//
+//  Created by mac on 2018/6/18.
+//  Copyright © 2018年 swift. All rights reserved.
+//
+
+import XCTest
+@testable import DouYuZB
+
+class DouYuZBTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
